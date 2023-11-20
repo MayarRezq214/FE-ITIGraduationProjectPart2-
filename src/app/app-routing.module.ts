76ea04210532f999ff37b6f6347042c775c5c37a
MayarRezq214 <mayar.rezq214@gmail.com>
2023-11-20 22:31:10 +0200

    initial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FormsComponent } from './forms/forms.component';
+import { FormLayoutComponent } from './form-layout/form-layout.component';
+import { GenralTablesComponent } from './genral-tables/genral-tables.component';
+import { DataTablesComponent } from './data-tables/data-tables.component';
+import { DoctorProfileComponent } from './doctor-profile/doctor-profile.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '' , component:DashboardComponent},
+  {path: 'header',component:HeaderComponent},
+  {path: 'footer' , component:FooterComponent},
+  {path: 'sidebar' , component:SidebarComponent},
+  {path: 'dashboard' , component:DashboardComponent},
+  {path : 'forms' , component : FormsComponent},
+  {path : 'formsLayout' , component : FormLayoutComponent},
+  {path : 'genralTables' , component : GenralTablesComponent},
+  {path : 'dataTables' , component : DataTablesComponent},
+  {path : 'doctorProfile' , component : DoctorProfileComponent},
+  {path : 'login' , component : LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FormsComponent } from './forms/forms.component';
+import { FormLayoutComponent } from './form-layout/form-layout.component';
+import { GenralTablesComponent } from './genral-tables/genral-tables.component';
+import { DataTablesComponent } from './data-tables/data-tables.component';
+import { DoctorProfileComponent } from './doctor-profile/doctor-profile.component';
+import { LoginComponent } from './login/login.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent,
+    DashboardComponent,
+    FormsComponent,
+    FormLayoutComponent,
+    GenralTablesComponent,
+    DataTablesComponent,
+    DoctorProfileComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
