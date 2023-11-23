77c6e42001eee8bdcc2973f026a5b29ea70a15f6
MayarRezq214 <mayar.rezq214@gmail.com>
2023-11-23 14:19:43 +0200

    get all doctors

--- src/app/services/admin.service.ts
+++ src/app/services/admin.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { GetAdminByPhoneNumberDto } from '../types/GetAdminByPhoneNumberDto';
+import { GetAllSpecializationForAdminDto } from '../types/GetAllSpecializationForAdminDto';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,10 @@ export class AdminService {
   constructor(private client: HttpClient) { }
   public getAdminByPhoneNumber(phoneNumber: string): Observable<GetAdminByPhoneNumberDto>{
     return this.client.get<GetAdminByPhoneNumberDto>(`https://localhost:7267/api/Admins/Admin/${phoneNumber}`)
+  }
+
+  public getAllSpecializationsAndDoctors(): Observable<GetAllSpecializationForAdminDto[]>{
+    return this.client.get<GetAllSpecializationForAdminDto[]>(`https://localhost:7267/api/Admins/Specializations`)
   }
 
 }
